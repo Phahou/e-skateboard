6e75bb171ccdc5536fc17b2f754792c19433e9bb
Phahou <38053767+Phahou@users.noreply.github.com>
2022-04-29 19:36:05 +0200

    Make code a bit clearer

--- src/nunchuck-transmitter.ino
+++ src/nunchuck-transmitter.ino
@@ -3,45 +3,45 @@
 #include <VirtualWire.h>
 
 void setup() {
+	 Serial.begin(9600);
  
-  Serial.begin(9600);
- 
-  // Nunchuk initialisieren und Joystick auf Mittelposition kalibrieren
-  vw_set_tx_pin(12);          // Sets pin D12 as the TX pin
-  vw_set_ptt_inverted(true);  // Required for DR3100
-  vw_setup(4000);         // Bits per sec
-  nunchuk_init();
-  delay(100);
-  nunchuk_calibrate_joy();
-  delay(100);
+	// Nunchuk initialisieren und Joystick auf Mittelposition kalibrieren
+	vw_set_tx_pin(12);					// Sets pin D12 as the TX pin
+	vw_set_ptt_inverted(true);	// Required for DR3100
+	vw_setup(4000);				 // Bits per sec
+	nunchuk_init();
+	delay(100);
+	nunchuk_calibrate_joy();
+	delay(100);
 }
  
 void loop() {
  
-  // Daten (6 Byte) vom Nunchuk Controller auslesen
-  nunchuk_get_data();
-  // Die einzelnen Werte stehen jetzt in Funktionen der WiiNunchuck-Library zur Verfügung
-  if(nunchuk_cbutton()) {
-    unsigned char msg =nunchuk_cjoy_y();
-    vw_send((uint8_t *)&msg, sizeof(uint8_t));
-    Serial.print("Send: int:");
-    Serial.print(msg);
-    
-  } else if(!(nunchuk_cbutton())) {
-      const unsigned char msg = 0;              //Stop signal
-      vw_send((uint8_t *)&msg, sizeof(uint8_t)); //Sending the stop message
-      Serial.print("Send(!C): ");
-      Serial.println(msg);
-  }
-  if (nunchuk_zbutton()){
-    /* do nothing aka hold the speed */
-    Serial.print("if Z\n");
-    
-    //while(nunchuk_cbutton()){ delay(100);};
-  }
-  char buffer[25];
-  //Serial.print("buff: ");
-  //sprintf(buffer, "X:%3d Y:%3d Z:%1d C:%1d", nunchuk_cjoy_x(), nunchuk_cjoy_y(), nunchuk_zbutton(), nunchuk_cbutton());
-  //Serial.println(buffer);
-  delay(50);
+	// Daten (6 Byte) vom Nunchuk Controller auslesen
+	nunchuk_get_data();
+	
+	// Die einzelnen Werte stehen jetzt in Funktionen der WiiNunchuck-Library zur Verfügung
+	if(nunchuk_cbutton()) {
+		unsigned char msg =nunchuk_cjoy_y();
+		vw_send((uint8_t *)&msg, sizeof(uint8_t));
+		Serial.print("Send: int:");
+		Serial.print(msg);
+		
+	} else if(!(nunchuk_cbutton())) {
+			const unsigned char msg = 0;					//Stop signal
+			vw_send((uint8_t *)&msg, sizeof(uint8_t));		//Sending the stop message
+			Serial.print("Send(!C): ");
+			Serial.println(msg);
+	}
+	if (nunchuk_zbutton()){
+		/* do nothing aka hold the speed */
+		Serial.print("if Z\n");
+		
+		//while(nunchuk_cbutton()){ delay(100);};
+	}
+	char buffer[25];
+	//Serial.print("buff: ");
+	//sprintf(buffer, "X:%3d Y:%3d Z:%1d C:%1d", nunchuk_cjoy_x(), nunchuk_cjoy_y(), nunchuk_zbutton(), nunchuk_cbutton());
+	//Serial.println(buffer);
+	delay(50);
 }
--- src/receiverwithmotor.ino
+++ src/receiverwithmotor.ino
@@ -9,61 +9,67 @@
 // $Id: receiver.pde,v 1.3 2009/03/30 00:07:24 mikem Exp $
 
 #include <VirtualWire.h>
-int count;
+int msg_nr;
 
 #include <ServoTimer2.h>
-ServoTimer2 firstESC; //Create as much as Servoobject you want. You can controll 2 or more Servos at the same time
-int value; // set values you need to zero motor reacts between 700 and 2000 ms
+ServoTimer2 mainESC;    // Could be multiple servos to control
+#define MOTOR_STOP_TIMING 700
+int esc_timing = 700;   // set esc_timings you need to zero motor reacts between 700 and 2000 ms
 
-void setup()
-{
-    Serial.begin(9600);  // Debugging only
-    firstESC.attach(9);    // attached to pin 9 I just do this with 1 Servo
-    delay(1);
-    Serial.println("setup"); //Prints "Setup" to the serial monitor
-    vw_set_rx_pin(12);       //Sets pin D12 as the RX Pin
-    vw_set_ptt_inverted(true); // Required for DR3100
-    vw_setup(4000);      // Bits per sec
-    vw_rx_start();       // Start the receiver PLL running
+void setup() {
+	Serial.begin(9600);        // Baud rate
+    // init esc pins
+	mainESC.attach(9);         // hook motor up to pin 9. Ref: https://pinout.xyz
+	delay(1);
+	Serial.println("setup");
+
+    // wireless setup
+	vw_set_rx_pin(12);          // Sets pin D12 as the RX Pin
+	vw_set_ptt_inverted(true);  // Required for DR3100
+	vw_setup(4000);             // Bits per sec
+	vw_rx_start();              // Start the receiver PLL running
 }
 
-void loop()
-{
-    uint8_t buf[VW_MAX_MESSAGE_LEN];
-    uint8_t buflen = VW_MAX_MESSAGE_LEN;
-    if (vw_get_message(buf, &buflen)) // Non-blocking
-    {
-      int i;
-      digitalWrite(13, true);  // Flash a light to show received good message
-      // Message with a good checksum received, dump it.
-      Serial.println("");
-      Serial.print("Got: ");
-      for (i = 0; i < buflen; i++)
-      {
-        unsigned char c = (buf[i]);
-        if (c==0) {
-          firstESC.write(700); //stops the motor
-          Serial.print("c = 0 ");
-          
-        } else if ((c<=136)&&(c>=130)){ Serial.print(" doing nothing ");//change nothing
-        } else if (c>=137) {
-            value=value+5;
-            firstESC.write(value);
-            Serial.print("val = +10 ");
-            
-        } else if (c<=129) {
-          value=value-5;
-          firstESC.write(value);
-          Serial.print("val = -10 ");
-        }
+void loop() {
+	uint8_t buf[VW_MAX_MESSAGE_LEN] = { 0 };
+
+    // get a message
+	if (vw_get_message(buf, &VW_MAX_MESSAGE_LEN)) { // Non-blocking
+		int i=0;
+		digitalWrite(13, true);	// Flash a light to show it received a message
         
-        Serial.print(c);
-        Serial.print(" ");
-      }
-        count++;
-        Serial.print(count);
-        Serial.print("    ");
-        Serial.println(value);
-        digitalWrite(13, false);
-    }
+        // I dont remember what I tried to do here
+		for (i = 0; i < VW_MAX_MESSAGE_LEN; i++) {
+    		unsigned char c = buf[i];
+            char* msg=0;
+    		if (c==0) {
+	    		mainESC.write(MOTOR_STOP_TIMING); //stops the motor
+                msg = "c == 0";
+			
+    		} else if ((c<=136)&&(c>=130)){
+    			msg = "doing nothing";
+    			
+    		} else if (c>=137) {
+    			esc_timing += 5;
+    			mainESC.write(esc_timing);
+    			msg = "val = +10";
+    			
+    		} else if (c<=129) {
+    			esc_timing -= 5;
+    			mainESC.write(esc_timing);
+    			msg = "val = -10 ";
+    		}
+            Serial.print("Status: ");
+            Serial.print(msg);
+            Serial.print(" | c =")
+    		Serial.print(c);
+	    	Serial.print(" ");
+		}
+		msg_nr++;
+		Serial.print("Got: ");
+		Serial.print(msg_nr);
+		Serial.print(" ");
+		Serial.println(esc_timing);
+		digitalWrite(13, false);
+	}
 }
